13f586c6813cd94e26e1ea3224767b847b4b7998
ArnRso <adrien.russo@gmail.com>
2019-04-18 22:29:56 +0200

    Composing Components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,8 @@ class Counter extends Component {
   state = {
     count: 0
   };
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -13,7 +14,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
